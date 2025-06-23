649d5b495b07e4182a78b2b4d5d566464290aebf
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2025-06-23 13:57:33 +0200

    Update audio.py

--- audio.py
+++ audio.py
@@ -1,26 +1,19 @@
 import essentia.standard as ess
 import magic, librosa, numpy
 
-# Do not bother if the file is not even audio
 def isAudioFile(FilePath):
     mime = magic.from_file(FilePath, mime=True)
     return(mime.startswith('audio'))
 
-# Throw a wild guess if essentia is not confident
-def getTempoLibrosa(AudioFile):
+def getKey(AudioFile):
+    keys = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
     y, sr = librosa.load(AudioFile)
-    tempo, beatFrames = librosa.beat.beat_track(y=y, sr=sr)
-    if isinstance(tempo, numpy.ndarray):
-        return(round(tempo.item()))
-    else:
-        return(0)
+    chromagram = librosa.feature.chroma_stft(y=y, sr=sr)
+    estimatedKey = keys[numpy.argmax(numpy.mean(chromagram, axis=1))]
+    return(estimatedKey)
 
-# Get the tempo using essentia
 def getTempo(AudioFile):
     audio = ess.MonoLoader(filename=AudioFile)()
     rhythmExtractor = ess.RhythmExtractor2013(method="multifeature")
     bpm, beats, beatsConfidence, _, beatsIntervals = rhythmExtractor(audio)
-    if beatsConfidence >= 1.5:
-        return(round(bpm))
-    else:
-        return(getTempoLibrosa(AudioFile))
+    return(round(bpm))
